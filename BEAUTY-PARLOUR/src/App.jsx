a75884d17e3444bed219d17d5fcf6a70d394cdda
Vedsutariya <vedsutariya66@gmail.com>
2025-06-05 17:01:42 +0530

    update||05-06

--- BEAUTY-PARLOUR/src/App.jsx
+++ BEAUTY-PARLOUR/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/header/Header'
 import Navbar from './components/header/navbar'
 import SimpleSlider from './components/header/slider'
+import Service from './components/main/service'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <Navbar/>
     <SimpleSlider/>
+    <Service/>
     </>
   )
 }
---
+++ BEAUTY-PARLOUR/src/components/main/service.jsx
@@ -0,0 +1,18 @@
+const Service = () => {
+    return (
+        <>
+            <main>
+                <div className="bg-[#e2e2e2] pt-5">
+                    <h2 className="flex justify-center text-4xl text-bold font-[800] tracking-wider">TRENDSETTING SERVICES</h2>
+                    <div className="flex justify-center pt-4">
+                        <div className="bg-red-500 w-[35px] h-[10px]"></div>
+                    </div>
+                    <p className="flex justify-center text-[17px] text-center pt-5">Our runway experts have curated some of the most popular services at Lakmē Salon, just for you.<br />
+                        Take your pick and head to your nearest salon and get started on your makeover!</p>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default Service;
